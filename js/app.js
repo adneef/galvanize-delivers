3d0e83db9f0b2b0de17345f83f23fe96e30f2da7
Adam Neef <adneef@gmail.com>
2017-08-18 09:58:59 -0600

    production model

--- js/app.js
+++ js/app.js
@@ -1,7 +1,31 @@
-document.addEventListener('DOMContentLoaded', function(){
+$(document).ready(function() {
+  console.log('ready!')
+  let subtotal = 0
+  let tax = 0
+  let total = 0
 
-  
+$('.orderBtn').click(function(){
+  // get item and price, make new td's and new tr,
+  // append all together and append to table body
 
+  let item = $(event.target).attr('id')
+  let price = $(event.target).val()
+  let td1 = $('<td>').text(item)
+  let td2 = $('<td>').text('$' + price)
+  let tr = $('<tr>')
+  tr.append(td1, td2)
+  $('.tableBody').append(tr)
 
+  // update subtotal, tax amount, and total for each click
+
+  subtotal += Number(price)
+  tax = subtotal * .0885
+  total = subtotal + tax
+
+  $('#subtotal').text('$' + (subtotal.toFixed(2)))
+  $('#tax').text('$' + tax.toFixed(2))
+  $('#total').text('$' + total.toFixed(2))
+
+})
 
 })
