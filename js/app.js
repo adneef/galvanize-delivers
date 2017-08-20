f77a29ecdda4c9d5a340c83eebde974eaa5af102
Adam Neef <adneef@gmail.com>
2017-08-20 10:32:53 -0500

    several updates, including qty in cart

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
   let subtotal = 0
   let tax = 0
   let total = 0
+  let cart = []
 
 $('.orderBtn').click(function(){
   // get item and price, make new td's and new tr,
@@ -10,12 +11,25 @@ $('.orderBtn').click(function(){
 
   let item = $(event.target).attr('id')
   let price = $(event.target).val()
-  let td1 = $('<td>').text(item)
-  let td2 = $('<td>').text('$' + price)
-  let tr = $('<tr>')
-  tr.append(td1, td2)
-  $('.tableBody').append(tr)
 
+  console.log(item);
+
+  if(cart.includes(item)) {
+
+  let clickedQty = Number($(`td:contains(${item})`).next().text())
+
+  $(`td:contains(${item})`).next().text(clickedQty+=1)
+
+  } else {
+  let tdItem = $('<td>').text(item)
+  let tdQty = $('<td>').text(1)
+  let tdPrice = $('<td>').text('$' + price)
+  let tr = $('<tr>')
+  tr.append(tdItem, tdQty, tdPrice)
+  $('.tableBody').append(tr)
+  cart.push(item)
+
+  }
   // update subtotal, tax amount, and total for each click
 
   subtotal += Number(price)
@@ -25,6 +39,38 @@ $('.orderBtn').click(function(){
   $('#subtotal').text('$' + (subtotal.toFixed(2)))
   $('#tax').text('$' + tax.toFixed(2))
   $('#total').text('$' + total.toFixed(2))
+
+})
+
+// submitBtn functionality and modal call
+
+$('.submitBtn').click(function(){
+  if (cart.length === 0) {
+    $('#myModal').modal('toggle')
+  } else if(cart.length > 0){
+    $('.modal-body').children().remove()
+    let orderClone = $('.orderAndInfo').clone()
+    orderClone.find('button').remove()
+    orderClone.find('aside').remove()
+    orderClone.removeClass('col-md-4')
+    orderClone.find('table').css('width', '95%')
+    $('.modal-body').append(orderClone)
+    $('#modalConfirm').text('Confirm Order')
+    console.log($('#modalConfirm'));
+    $('#modalCancel').text('Cancel')
+    $('#myModal').modal('toggle')
+  }
+})
+
+modal confirm update
+
+$('#modalConfirm').click(function(){
+  $('.modalBody').children().remove()
+  $('.modalBody').append($('<p/>'))
+  $('.modalBody').children().text(`Your order will arrive as soon as this site exists!
+    If you gave us an email, your receipt has been emailed.`)
+
+  $('#myModal2').modal('toggle')
 
 })
 
